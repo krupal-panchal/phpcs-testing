bc0cf1d086fd83b96db2c97d45c9fcb957f945ad
Krupal <krupalpanchal1506@gmail.com>
2023-05-24 19:49:31 +0530

    initial commit

---
+++ class-wordpress-test.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file basically created for Testing a WordPress Coding standard
+ */
+class WordPress_Test {
+
+	/**
+	 * Name var
+	 *
+	 * @var string $name Name
+	 */
+	public string $name = 'John';
+
+	/**
+	 * Age var
+	 *
+	 * @var string $age Age
+	 */
+	public int $age = 26;
+
+	/**
+	 * Instantiate the object.
+	 */
+	public function __construct() {}
+
+	/**
+	 * Method to check if age valid or not.
+	 *
+	 * @param int $age Age.
+	 *
+	 * @return bool
+	 */
+	public function is_age_valid( $age ) : bool {
+
+		if ( $age > 25 ) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+}
